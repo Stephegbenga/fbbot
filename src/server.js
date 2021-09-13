42007d996326ea8195b3e1b9eb6adf3e7af3aa13
Fredrick Brudge <fredrickbrudge001@gmail.com>
2021-09-13 19:23:21 +0000

    update

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //init web routes
 initWebRoutes(app);
 
-let port = process.env.port || 8080;
+let port = process.env.PORT || 8080;
 
 app.listen(port, ()=>{
     console.log("App is listening at: "+port);
