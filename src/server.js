77f3168b36c6e2d313040ad630c0cfc8b7ec6a53
Fredrick Brudge <fredrickbrudge001@gmail.com>
2021-09-13 19:11:43 +0000

    deploy heroku

---
+++ src/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+import express from "express"
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+
+
+let app = express();
+//config viewEngine
+viewEngine(app);
+// config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+//init web routes
+initWebRoutes(app);
+
+let port = process.env.port || 8080;
+
+app.listen(port, ()=>{
+    console.log("App is listening at: "+port);
+});
